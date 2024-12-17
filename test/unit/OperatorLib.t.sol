35e676ee8e63b6b858c10dc392647f1167ef10f9
steven <steven.nevins.js@gmail.com>
2024-12-17 17:02:05 -0500

    chore: remove comments

--- test/unit/OperatorLib.t.sol
+++ test/unit/OperatorLib.t.sol
@@ -12,17 +12,13 @@ contract OperatorLibTest is Test {
         uint256 index = 1;
         OperatorLib.Operator memory operator = OperatorLib.createOperator(index);
 
-        // Check that the operator's VM wallet address is non-zero
         assertTrue(operator.key.addr != address(0), "VM wallet address should be non-zero");
 
-        // Check that the operator's BLS private key is non-zero
         assertTrue(operator.signingKey.privateKey != 0, "BLS private key should be non-zero");
 
-        // Check that the operator's BLS public key G1 is non-zero
         assertTrue(operator.signingKey.publicKeyG1.X != 0 || operator.signingKey.publicKeyG1.X != 0, "BLS public key G1 X should be non-zero");
         assertTrue(operator.signingKey.publicKeyG1.Y != 0 || operator.signingKey.publicKeyG1.Y!= 0, "BLS public key G1 Y should be non-zero");
 
-        // Check that the operator's BLS public key G2 is non-zero
         assertTrue(operator.signingKey.publicKeyG2.X[0] != 0 || operator.signingKey.publicKeyG2.X[1] != 0, "BLS public key G2 X should be non-zero");
         assertTrue(operator.signingKey.publicKeyG2.Y[0] != 0 || operator.signingKey.publicKeyG2.Y[1] != 0, "BLS public key G2 Y should be non-zero");
     }
@@ -41,8 +37,6 @@ contract OperatorLibTest is Test {
             messagePoint,
             operator.signingKey.publicKeyG2
         );
-
-        // Check that the signature is valid
         assertTrue(isValid, "Signature should be valid");
     }
 }
